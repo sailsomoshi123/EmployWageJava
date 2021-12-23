2ab5691386a605099015aca93050c8f3a6a080f8
Sail Somoshi <sailsomoshi11@gmail.com>
2021-12-23 17:34:01 +0530

    daily employee wage computation

--- EmpWage.java
+++ EmpWage.java
@@ -4,10 +4,16 @@ public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("welcome to empwage computation");
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		int empWages = 0;
+		int empHour = 0;
 		double empCheck = Math.floor(Math.random() *10)%2;
 		if (empCheck == IS_FULL_TIME)
-			System.out.println("Employee is Present");
+			empHour = 8;
 		else
-			System.out.println("employee is Absent");
+		    empHour = 0;
+	empWages = empHour * EMP_RATE_PER_HOUR;
+	System.out.println("employee wage is " + empWages);
+		
 	}
 }
