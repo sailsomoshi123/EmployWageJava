a7cb581a66d53a83499cefcfd09fbfac64fe4b61
Sail Somoshi <sailsomoshi11@gmail.com>
2021-12-23 17:45:01 +0530

    part time wage also added

--- EmpWage.java
+++ EmpWage.java
@@ -4,12 +4,15 @@ public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("welcome to empwage computation");
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
 		int empWages = 0;
 		int empHour = 0;
-		double empCheck = Math.floor(Math.random() *10)%2;
+		double empCheck = Math.floor(Math.random() *10)%3;
 		if (empCheck == IS_FULL_TIME)
 			empHour = 8;
+		else if (empCheck == IS_PART_TIME)
+			empHour = 4;
 		else
 		    empHour = 0;
 	empWages = empHour * EMP_RATE_PER_HOUR;
