e9371c96ffcaecd0d07749ef7e96ec854a54fd3b
Sail Somoshi <sailsomoshi11@gmail.com>
2021-12-23 17:59:22 +0530

    use of switch case statement

--- EmpWage.java
+++ EmpWage.java
@@ -3,18 +3,22 @@ package com.WageComputatiom;
 public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("welcome to empwage computation");
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
+		final int IS_FULL_TIME = 1;
+		final int IS_PART_TIME = 2;
+		final int EMP_RATE_PER_HOUR = 20;
 		int empWages = 0;
 		int empHour = 0;
-		double empCheck = Math.floor(Math.random() *10)%3;
-		if (empCheck == IS_FULL_TIME)
-			empHour = 8;
-		else if (empCheck == IS_PART_TIME)
-			empHour = 4;
-		else
-		    empHour = 0;
+		int empCheck = (int) Math.floor(Math.random() *10)%3;
+		switch (empCheck) {
+			case IS_FULL_TIME:
+				empHour = 8;
+				break;
+			case IS_PART_TIME:
+				empHour = 4;
+				break;
+			default:
+				empHour = 0;
+				}
 	empWages = empHour * EMP_RATE_PER_HOUR;
 	System.out.println("daily employee wage is " + empWages);
 		
