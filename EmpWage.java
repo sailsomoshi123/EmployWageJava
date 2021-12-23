6aebec282ffd92febc1789b41ecf5ce666b4c654
Sail Somoshi <sailsomoshi11@gmail.com>
2021-12-23 16:51:28 +0530

    to check employee is present or absent

--- EmpWage.java
+++ EmpWage.java
@@ -3,5 +3,11 @@ package com.WageComputatiom;
 public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("welcome to empwage computation");
+		int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random() *10)%2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("employee is Absent");
 	}
 }
